06b4ecaebf942dbd5878496cebd81eaaf13307c7
Thanit16 <thanitpc888@gmail.com>
2024-01-22 03:10:31 +0700

    'step_up_wep'

--- web/src/pages/Header.js
+++ web/src/pages/Header.js
@@ -24,7 +24,9 @@ const Header = () => {
   const theme = useTheme();
   const isMatch = useMediaQuery(theme.breakpoints.down("md"));
   const [openPopup, setOpenPopup] = useState(false);
-
+  const [isSignIn, setIsSignIn] = useState(false); 
+  /* const [isSignUp, setIsSignIn] = useState(false); /*  */
+ 
   const closePopup = (event, reason) => {
     if (reason === 'backdropClick') {
         setOpenPopup(false);
@@ -43,25 +45,28 @@ const Header = () => {
         </>
       ) : (
         <>
+        <img src={Logo} sx={{ transform: "scale(2)"}} ></img>
          <Tabs
-  sx={{ marginLeft: "0" ,marginRight:"auto"}}
+  sx={{ marginLeft: "auto" }}
   indicatorColor="secondary"
   textColor="inherit"
   value={value}
   onChange={(e, value) => setValue(value)}
 >
-  <Tab label="Service" component={Link} to="/service" sx={{paddingLeft:"0"}}/>
+
+  <Tab label="Home" component={Link} to="/" sx={{paddingLeft:"0"}}/>
+  <Tab label="Service" component={Link} to="/service"/>
   <Tab label="My Service" component={Link} to="/myservice" />
   <Tab label="My Work" component={Link} to="/mywork" />
   <Tab label="My Order" component={Link} to="/myorder" sx={{paddingRight:"0"}}/>
 </Tabs>
-          <img src={Logo} sx={{ transform: "scale(2)"}} style={{alignItems:"center",justifyContent:"center"}} alt="logo"></img>
+        
           <div style={{marginLeft:"auto"}} >
             
-            <Button variant="contained" onClick={()=>{setOpenPopup(true);}} sx={{background:"#472874",borderRadius:"30px"}}>
+            <Button variant="contained" onClick={()=>{setOpenPopup(true);setIsSignIn(true);}} sx={{background:"#472874",borderRadius:"30px"}}>
               Login
             </Button>
-            <Button sx={{ marginLeft: "10px",background:"#472874",borderRadius:"30px" }} variant="contained" >
+            <Button sx={{ marginLeft: "10px",background:"#472874",borderRadius:"30px" }} variant="contained"  onClick={()=>{setOpenPopup(true);setIsSignIn(false);}}>
               SignUp
             </Button>
           </div>
@@ -77,7 +82,7 @@ const Header = () => {
                 setOpenPopup={setOpenPopup}
                 closePopup={closePopup}
             >
-                <Login/>
+                <Login isSignIn={isSignIn}/>
             </Popup>
             <Outlet/>
     </React.Fragment>
