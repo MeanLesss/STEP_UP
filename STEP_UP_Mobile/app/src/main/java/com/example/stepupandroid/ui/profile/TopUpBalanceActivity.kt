ceca66cb69d8b7ea8fff88b857acecbdc80e553c
Pitou <pitoutangly@gmail.com>
2024-01-23 16:54:13 +0700

    add top up balance

---
+++ STEP_UP_Mobile/app/src/main/java/com/example/stepupandroid/ui/profile/TopUpBalanceActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stepupandroid.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stepupandroid.R
+
+class TopUpBalanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_up_balance)
+    }
+}
