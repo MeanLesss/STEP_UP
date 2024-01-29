c63f3efdccab7dfc7b9822e36f226890c1e2a58b
Pitou <pitoutangly@gmail.com>
2024-01-29 11:28:26 +0700

    add download to client upload

--- STEP_UP_Mobile/app/src/main/java/com/example/stepupandroid/adapter/ResourceAdapter.kt
+++ STEP_UP_Mobile/app/src/main/java/com/example/stepupandroid/adapter/ResourceAdapter.kt
@@ -3,15 +3,27 @@ package com.example.stepupandroid.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.stepupandroid.R
+import com.example.stepupandroid.model.Attachment
+import com.example.stepupandroid.model.DownloadAttachment
 
-class ResourceAdapter(private val resources: MutableList<String>) :
+class ResourceAdapter(private val resources: MutableList<DownloadAttachment>) :
     RecyclerView.Adapter<ResourceAdapter.ViewHolder>() {
 
+    private var listener: OnDownloadClick? = null
+    interface OnDownloadClick {
+        fun onDownloadClick(attachment: DownloadAttachment)
+    }
+
+    fun setOnDownloadClick(listener: OnDownloadClick) {
+        this.listener = listener
+    }
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textAttachmentName: TextView = view.findViewById(R.id.attachmentName)
+        val downloadButton: ImageView = view.findViewById(R.id.download)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,7 +34,10 @@ class ResourceAdapter(private val resources: MutableList<String>) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val resource = resources[position]
-        holder.textAttachmentName.text = resource
+        holder.textAttachmentName.text = resource.fileName
+        holder.downloadButton.setOnClickListener {
+            listener?.onDownloadClick(resource)
+        }
     }
 
     override fun getItemCount() = resources.size
